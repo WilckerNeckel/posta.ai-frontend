d725467b0f49173b02e4e67caeba23f11a47c25e
WilckerNeckel <wilckerwrsn@gmail.com>
2025-04-21 21:00:25 -0400

    feat: create two theme files

---
+++ src/themes/jsonTheme.ts
@@ -0,0 +1,20 @@
+// src/theme/colors.ts
+export const palette = {
+    primary: {
+      main: "#441B16",
+      400: "#2D120F", // optional, same as main
+    },
+    secondary: {
+      main: "#815743",
+    },
+    background: {
+      default: "#E2D3CA",
+    },
+    divider: {
+      default: "#A09085",
+    },
+    accent: {
+      default: "#EFBF04",
+    },
+  };
+  
---
+++ src/themes/muiTheme.ts
@@ -0,0 +1,34 @@
+// src/theme/theme.ts
+import { createTheme } from '@mui/material/styles';
+
+export const muiTheme = createTheme({
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    h1: {
+      fontSize: "2.5rem",
+      fontWeight: 700,
+    },
+    h2: {
+      fontSize: "2rem",
+      fontWeight: 600,
+    },
+    body1: {
+      fontSize: "1rem",
+      fontWeight: 400,
+    },
+    button: {
+      textTransform: "none", // remove uppercase on buttons
+    },
+  },
+  palette: {
+    primary: {
+      main: "#441B16",
+    },
+    secondary: {
+      main: "#815743",
+    },
+    background: {
+      default: "#E2D3CA",
+    },
+  },
+});
